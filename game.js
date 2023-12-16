ad9271d31a9b466285fd527eb4cdfffb1ecff12b
picuscalin <picuscalin@yahoo.com>
2023-12-16 13:10:18 +0200

    finished the score function

--- game.js
+++ game.js
@@ -1,57 +1,118 @@
 let score=0;
 let computerScore=0;
 
+const rockButton=document.querySelector('.rock');
+const paperButton=document.querySelector(".paper");
+const scissorsButton=document.querySelector(".scissors");
+const outcome=document.querySelector(".score");
+const p=document.createElement("p");
+
+
+
 function getComputerChoice(){
     const options=["rock","paper" ,"scissors"];
-    const choice=options[Math.floor(Math.random())*options.length]
+    const choice=Math.floor(Math.random() *3);
+    const pcChoice=options[choice]
+    
 
-    return choice;
+    return pcChoice;
 }
+
 
 function newRound(playerSelection, computerSelection){
-    if(playerSelection == computerSelection){
-        return "tie";
+    if(playerSelection ===computerSelection){
+        
+       
+        p.innerText="tie";
+        outcome.appendChild(p)
+        
+        
     }
-    if(playerSelection=="rock" && computerSelection=="paper"){
-        return `you lose ${playerSelection} weaker than ${computerSelection}`;
+    else if(playerSelection==="rock" && computerSelection==="paper"){
         computerScore ++;
-    }
-    else if(playerSelection=="rock" && computerSelection=="scissors"){
-        return `you win ${playerSelection} stronger than ${computerSelection}`;
-        score ++;
-    }
-    if(playerSelection=="paper" && computerSelection=="scissors"){
+        p.innerText=`you lose ${playerSelection} weaker than ${computerSelection}`;
+        outcome.appendChild(p)
+        
         return `you lose ${playerSelection} weaker than ${computerSelection}`;
-        computerScore ++;
+        
     }
-    else if(playerSelection=="paper" && computerSelection=="rock"){
-        return `you win ${playerSelection} stronger than ${computerSelection}`;
+    else if(playerSelection==="rock" && computerSelection==="scissors"){
         score ++;
+        p.innerText=`you win ${playerSelection} stronger than ${computerSelection}`;
+        outcome.appendChild(p)
+        return `you win ${playerSelection} stronger than ${computerSelection}`;
+        
     }
-    if(playerSelection=="scissors" && computerSelection=="rock"){
+    else if(playerSelection==="paper" && computerSelection==="scissors"){
+        computerScore ++;
+        p.innerText=`you lose ${playerSelection} weaker than ${computerSelection}`;
+        outcome.appendChild(p)
         return `you lose ${playerSelection} weaker than ${computerSelection}`;
-        computerScore ++;
+        
     }
-    else if(playerSelection=="scissors" && computerSelection=="paper"){
-        return `you win ${playerSelection} stronger than ${computerSelection}`;
+    else if(playerSelection==="paper" && computerSelection==="rock"){
         score ++;
+        p.innerText=`you win ${playerSelection} stronger than ${computerSelection}`;
+        outcome.appendChild(p)
+        return `you win ${playerSelection} stronger than ${computerSelection}`;
+       
+    }
+    else if(playerSelection==="scissors" && computerSelection==="rock"){
+        computerScore ++;
+        p.innerText=`you lose ${playerSelection} weaker than ${computerSelection}`;
+        outcome.appendChild(p)
+        return `you lose ${playerSelection} weaker than ${computerSelection}`;
+        
+    }
+    else if(playerSelection==="scissors" && computerSelection==="paper"){
+        score ++;
+        p.innerText=`you win ${playerSelection} stronger than ${computerSelection}`;
+        outcome.appendChild(p)
+        return `you win ${playerSelection} stronger than ${computerSelection}`;
+        
     }
 }
 
-function game(){
-    for(let i=0;i<5;i++){
-        const enter=prompt("emter choice");
-        const compChoice=getComputerChoice();
-        console.log(newRound(enter,compChoice));
-    }
 
-    if(score<computerScore){
-        return "lose";
-    }else if(score>computerScore){
-        return "win";
-    }else{
-        return "tie";
+rockButton.addEventListener("click", ()=>{
+    const pcChoice=getComputerChoice();
+    const playerSelection="rock";
+    newRound(playerSelection,pcChoice);
+    whoWon(score,computerScore);
+})
+
+
+paperButton.addEventListener("click", ()=>{
+    const pcChoice=getComputerChoice();
+    const playerSelection="paper";
+    newRound(playerSelection,pcChoice);
+    whoWon(score,computerScore);
+})
+
+
+scissorsButton.addEventListener("click", ()=>{
+    const pcChoice=getComputerChoice();
+    const playerSelection="scissors";
+    newRound(playerSelection,pcChoice);
+    whoWon(score,computerScore);
+})
+
+
+function whoWon(urScore,dskScore){
+    let h2=document.createElement("h2");
+    if(urScore===5){
+        h2.innerHTML=`you won ${urScore} is greater than ${dskScore}`
+        outcome.append(h2);
     }
+    if(dskScore===5){
+        h2.innerHTML=`you lost ${urScore} is lower than ${dskScore}`
+        outcome.append(h2);
+    }
+    
 }
 
-console.log(game());
+
+
+
+
+
